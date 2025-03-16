5188d21c512558a758f03eabdb5f8bf69508c918
SHARE IT HUB <156384686+shareithub@users.noreply.github.com>
2025-03-16 23:20:41 +0700

    Update izumi.py

--- modules/izumi.py
+++ modules/izumi.py
@@ -10,8 +10,8 @@ load_dotenv()
 
 def display_header():
     print(Style.BRIGHT + Fore.CYAN + "======================================")
-    print(Style.BRIGHT + Fore.CYAN + "            Izumi Module            ")
-    print(Style.BRIGHT + Fore.CYAN + "======================================\n")
+    print(Style.BRIGHT + Fore.CYAN + "            Izumi Bot                 ")
+    print(Style.BRIGHT + Fore.CYAN + "====================================\n")
 
 display_header()
 
